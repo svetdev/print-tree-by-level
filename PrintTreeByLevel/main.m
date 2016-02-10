0c6ba662233fd3744857e41b534572711017d0ef
Andrey Kasatkin <akasatkin@coocoo.com>
2016-02-10 13:08:40 -0500

    empty main

---
+++ PrintTreeByLevel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PrintTreeByLevel
+//
+//  Created by Andrey Kasatkin on 2/10/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        
+    }
+}
